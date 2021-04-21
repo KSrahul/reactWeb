c2d8d4243d3d340daace3e52559f1e3e03b44551
KSrahul <rahul.alam9873@gmail.com>
2021-04-21 15:33:34 +0530

    name changed

--- react_live/src/App.js
+++ react_live/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Alam Rahul Edit <code>src/App.js</code> and save to reload.
+          Alam Rahul Alam Rahul Alam Alam END <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
