33a91c8a901e1ed76a8ef3e1290f18eca60d578b
KSrahul <rahul.alam@eazydiner.com>
2021-07-18 21:09:24 +0530

    changes

--- react_live/src/App.jsx
+++ react_live/src/App.jsx
@@ -1,13 +1,15 @@
 // import logo from './logo.svg';
 import './Main.css';
 import Menu from './Components/Navbar/Menu';
-import Tabs from './Components/Tabs/Tabs'
+import Tabs from './Components/Tabs/Tabs';
+import FilterMain from './Components/Filter/FilterMain'
 
 function App() {
   return (
     <>
-      <Menu></Menu>
-      <Tabs></Tabs>
+      {/* <Menu></Menu>
+      <Tabs></Tabs> */}
+      <FilterMain></FilterMain>
     </>
   );
 }
--- react_live/src/Components/Tabs/Tabs.jsx
+++ react_live/src/Components/Tabs/Tabs.jsx
@@ -1,61 +1,43 @@
 import Tab from "./Tab.css";
-import React, { useState } from "react";
+import React, { useState, cloneElement } from "react";
 
-const Tabs = () => {    
-    const [boxState, changeBox] = useState({
-        activeBox : null,
-        boxUnique : [
-            {
-                id : 1, boxName : "Box1",
-            },
-            {
-                id : 2, boxName : "Box2",
-            },
-            {
-                id : 3, boxName : "Box3",
-            },
-            {
-                id : 4, boxName : "Box4",
-            },
-            {
-                id : 5, boxName : "Box5",
-            },
-            {
-                id : 6, boxName : "Box6",
-            },
-        ]
-    })
-    
-    
+const Tabs = () => {  
 
-    function boxClick(boxIndex) {
-        changeBox({ ...boxState, activeBox : boxState.boxUnique[boxIndex]})
-        console.log(boxState.boxUnique[boxIndex])
+    const [boxState, changeBox] = useState([]);
+    let allBoxes = [
+        {
+            id: 0,
+            boxName : 1
+        },
+        {
+            id: 1,
+            boxName : 2
+        },
+        {
+            id: 2,
+            boxName : 3
+        },
+        {
+            id: 3,
+            boxName : 4
+        },
+    ]
+    
+    function boxClick(clickEle, clickIndex){
+        console.log(clickEle.id)
+        console.log(clickIndex)
     }
-
-    function toggleClass(boxIndex){
-        if(boxState.boxUnique[boxIndex] == boxState.activeBox){
-            return "tabs_boxes box_activated";
-        }else{
-            return "tabs_boxes notactivated";
-        }
-    }
-    
 
     return(
         <>
             <div className="tabs_boxes_container">
                 <div className="tabs_internal">
                     {
-                        boxState.boxUnique.map((boxEle, boxIndex) =>{
+                        allBoxes.map((ele, boxIndex) =>{
                             return(
-                                <div key={boxIndex} className={toggleClass(boxIndex)} 
-                                    onClick={() => {
-                                        boxClick(boxIndex)  
-                                    }}
-                                >
-                                    {boxEle.boxName}
-                                </div>      
+                                <div key={boxIndex} onClick={() => boxClick(ele, boxIndex)} className="tabs_boxes">
+                                    Box{ele.boxName}
+                                </div>
                             )
                         })
                     }
