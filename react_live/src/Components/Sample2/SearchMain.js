8e688061195c18f522c5a6e566cfc9a7b63005b9
KSrahul <rahul.alam@eazydiner.com>
2021-09-14 09:09:25 +0530

    fixes

--- react_live/src/Components/Sample2/SearchMain.js
+++ react_live/src/Components/Sample2/SearchMain.js
@@ -1,17 +1,18 @@
-import React from 'react'
+import React, { useState } from 'react'
 import SearchCss from '../Sample2/Search.css'
 import SearchView from './SearchView'
 import SearchData from './SearchData'
 import SearchItems from './SearchItems'
 const Example2 = () => {
+    const [inputVal, inputValFun] = useState("");
 
-    const InputValChange = (event) => {
-        console.log(event.target.value)
+    const InputValChange = (valInput) => {
+        inputValFun(valInput.target.value)
     }
     return(
         <>
             <SearchItems allItems={SearchData}></SearchItems>
-            <SearchView InputValChange={InputValChange}></SearchView>
+            <SearchView InputValChange={InputValChange} toggleIcon={inputVal.length}></SearchView>
         </>
     )
 }
