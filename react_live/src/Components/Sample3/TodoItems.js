4e47ed3f8f8040e0536ed6a1fc6aea98dd6a26a3
KSrahul <rahul.alam@eazydiner.com>
2021-09-19 16:25:04 +0530

    fixes

--- react_live/src/Components/Sample3/TodoItems.js
+++ react_live/src/Components/Sample3/TodoItems.js
@@ -4,7 +4,18 @@ const TodoItems = (props) => {
             {
                 props.listName.map((itemName, itemIndex) =>{
                     return(
-                        <div key={itemIndex}>{itemName}</div>
+                        <div className="flex added_items" key={itemIndex}>
+                            <div>{itemName}</div>
+                            <div className="remove_item">
+                                <svg xmlns='http://www.w3.org/2000/svg' width='24' height='24' 
+                                    viewBox='0 0 24 24' fill='none' stroke='#ff3d46' strokeWidth='2' strokeLinecap='round' strokeLinejoin='round'>
+                                    <polyline points='3 6 5 6 21 6' />
+                                    <path d='M19 6v14a2 2 0 0 1-2 2H7a2 2 0 0 1-2-2V6m3 0V4a2 2 0 0 1 2-2h4a2 2 0 0 1 2 2v2' />
+                                    <line x1='10' y1='11' x2='10' y2='17' />
+                                    <line x1='14' y1='11' x2='14' y2='17' />
+                                </svg>
+                            </div>
+                        </div>
                     )
                 })
             }
--- react_live/src/Components/Sample3/TodoMain.js
+++ react_live/src/Components/Sample3/TodoMain.js
@@ -1,33 +1,58 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import {} from './Todo.css'
 import TodoItems from './TodoItems'
 const TodoMain = () => {
+    const inputFocus = useRef(null);
     const [inputVal, inputValFunc] = useState("")
-    const [addInputVal, addInputValFun] = useState([])
+    const [addInputVal, addInputValFun] = useState([]);
+    const ItemsAdding = addInputVal.concat(inputVal);
+
     const inputType = (event) => {
         inputValFunc(event.target.value)
     }
-    const addItems = () => {
-        // inputValFunc("");
-        // addInputVal.push(inputVal);
 
+    const addItems = () => {
         if(inputVal.length > 0){
             inputValFunc("");
-            addInputValFun(addInputVal.concat(inputVal))
+            addInputValFun(ItemsAdding)
         }
     }
+
+    const keyCheck = (event) => {
+        if(event.key === "Enter" && inputVal.length > 0){
+            inputValFunc("");
+            addInputValFun(ItemsAdding)
+        }
+    }
+
+    useEffect(() => {
+        inputFocus.current.focus();
+    }, [inputFocus])
       return(
           <>
             <div className="todo_main">
                 <div className="todo_text">TO DO LIST</div>
                 <div className="input_add relative">
-                    <input onChange={inputType} value={inputVal} type="text" placeholder="Add New Task" />
-                    <div className="add_btn" onClick={addItems}>+</div>
+                    <input 
+                        onChange={inputType}
+                        onKeyDown={keyCheck}
+                        value={inputVal}
+                        ref={inputFocus}
+                        type="text" 
+                        placeholder="Add New Task" 
+                    />
+
+                    <div className="add_btn" 
+                        onClick={addItems}>
+                        +
+                    </div>
                 </div>
                 {
                     addInputVal.length > 0 ? 
                         <div className="all_todo">
-                            <TodoItems listName={addInputVal}></TodoItems>
+                            <TodoItems 
+                                listName={addInputVal}>
+                            </TodoItems>
                         </div>
                     :false
                 }
