0aacb50610643f7fad7eb27d89394852df05a212
KSrahul <rahul.alam@eazydiner.com>
2021-09-21 14:08:34 +0530

    fixes

--- react_live/src/Components/Sample3/EditTodoModal.js
+++ react_live/src/Components/Sample3/EditTodoModal.js
@@ -3,8 +3,8 @@ const EditTodoModal = (props) => {
         <>
             <div className="quick_edit">
                 <div className="cross_sign pointer" onClick={props.removeEditText}>
-                    <svg stroke="white" fill="whiite" stroke-width="0" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg">
-                        <path fill="white" stroke="white" stroke-width="2" d="M3,3 L21,21 M3,21 L21,3"></path>
+                    <svg stroke="white" fill="whiite" strokeWidth="0" viewBox="0 0 24 24" height="30" width="30" xmlns="http://www.w3.org/2000/svg">
+                        <path fill="white" stroke="white" strokeWidth="2" d="M3,3 L21,21 M3,21 L21,3"></path>
                     </svg>
                 </div>
                 <div className="text_save pointer">
