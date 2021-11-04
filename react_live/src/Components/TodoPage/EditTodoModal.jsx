ad17d770a886a20855ecfbb76a506cfe66378702
KSrahul <rahul.alam@eazydiner.com>
2021-11-04 14:35:18 +0530

    edit update

--- react_live/src/Components/TodoPage/EditTodoModal.jsx
+++ react_live/src/Components/TodoPage/EditTodoModal.jsx
@@ -1,8 +1,16 @@
+import { useRef, useEffect } from "react"
+
 const EditTodoModal = (props) => {
+    const textAraFocus = useRef(null)
+
+    useEffect(() => {
+        // textAraFocus.current.select();
+        textAraFocus.current.focus();
+    }, [])
     return (
         <>
             <div className="flex edit_main">
-                <input className="edit_val" onChange={props.editTodoValue} onKeyDown={props.textareaEnter} type="text" defaultValue={props.todoObj.listName} />
+                <input className="edit_val" ref={textAraFocus} onChange={props.editTodoValue} onKeyDown={props.textareaEnter} type="text" defaultValue={props.todoObj.listName} />
                 <div className="flex ctn_items">
                     <div className="save_btn pointer" onClick={props.saveEditValue}>Save</div>
                     <div className="cancel_btn pointer" onClick={props.removeEditText}>Cancel</div>
